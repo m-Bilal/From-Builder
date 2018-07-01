bf821f8975d7b9484ac6a59bca18193a78017c11
Mohd Bilal <mohd.bilal1996@gmail.com>
2018-07-01 23:13:15 +0530

    Layout files and basic layouts

--- app/src/main/java/com/bilal/formbuilder/activity/MainActivity.java
+++ app/src/main/java/com/bilal/formbuilder/activity/MainActivity.java
@@ -1,13 +1,20 @@
-package com.bilal.formbuilder;
+package com.bilal.formbuilder.activity;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bilal.formbuilder.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewpager);
     }
 }
